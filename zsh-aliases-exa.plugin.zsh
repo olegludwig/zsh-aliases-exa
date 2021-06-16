c0cee0087e3b8ae68493779067572c354e1f9071
olegludwig <13472826+olegludwig@users.noreply.github.com>
2021-06-16 17:10:44 +0200

    Update zsh-aliases-exa.plugin.zsh

--- zsh-aliases-exa.plugin.zsh
+++ zsh-aliases-exa.plugin.zsh
@@ -10,8 +10,8 @@
 
 # general use
 alias ls='exa'                                                         # ls
-alias l='exa -lbF --git'                                               # list, size, type, git
-alias ll='exa -lbGF --git'                                             # long list
+alias l='exa -lbF --git --time-style=long-iso'                         # list, size, type, git
+alias ll='exa -lbGF --git --time-style=long-iso'                       # long list
 alias llm='exa -lbGF --git --sort=modified'                            # long list, modified date sort
 alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
 alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
